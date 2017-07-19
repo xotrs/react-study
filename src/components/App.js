a303bce4794cf9f90cfc2c0ee2cdbb81311d6e21
Lim Young Rok <xotrs1@naver.com>
2017-07-19 22:44:15 +0900

    Props 공부.

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,19 @@ class App extends React.Component {
     render(){
         return (
             <div>
-                <Header />
-                <Content />
+                <Header title={ this.props.headerTitle }/>
+                <Content title={ this.props.contentTitle }
+                          body={ this.props.contentBody }/>
             </div>
         );
     }
 }
+
+App.defaultProps = {
+    headerTitle: 'Default header',
+    contentTitle: 'Default contentTitle',
+    contentBody: 'Default contentBody'
+};
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 const rootElement = document.getElementById('root');
+// ReactDOM.render(<App headerTitle = "Welcome!"
+//                      contentTitle = "Stranger,"
+//                      contentBody = "Welcome to example app"/>, rootElement);
+ 
 ReactDOM.render(<App />, rootElement);
-
