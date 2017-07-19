2963c10b6469fc86196645c4bfa54c664602ca92
Lim Young Rok <xotrs1@naver.com>
2017-07-19 23:07:27 +0900

    컴포넌트 Iteration (반복) – Map

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import Header from './Header';
 import Content from './Content';
+import Contacts from './Contacts';
 
 class App extends React.Component {
     render(){
         return (
-            <div>
-                <Header title={ this.props.headerTitle }/>
-                <Content title={ this.props.contentTitle }
-                          body={ this.props.contentBody }/>
-            </div>
+                <Contacts />
         );
     }
 }
-
-App.defaultProps = {
-    headerTitle: 'Default header',
-    contentTitle: 'Default contentTitle',
-    contentBody: 'Default contentBody'
-};
 
 
 export default App;
