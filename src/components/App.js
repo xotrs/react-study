32df76fcf421e4d89f9a5a54ec7f4e1915606ccf
Lim Young Rok <xotrs1@naver.com>
2017-07-19 22:29:06 +0900

    Component 생성 및 모듈화 - Content, Header 컴포넌트 추가.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Header from './Header';
+import Content from './Content';
 
 class App extends React.Component {
     render(){
-
         return (
-                <h1>Hello React Skeleton</h1>
+            <div>
+                <Header />
+                <Content />
+            </div>
         );
     }
 }
