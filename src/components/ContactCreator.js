9dd8a7abb772e84d110d7edf9ce902229a480d6e
Lim Young Rok <xotrs1@naver.com>
2017-07-21 13:46:37 +0900

    setState 공부 중.

--- src/components/ContactCreator.js
+++ src/components/ContactCreator.js
@@ -4,21 +4,32 @@ class ContactCreator extends React.Component {
     constructor(props){
             super(props);
             this.state = {
-                contactData: {
-                    name: "",
-                    phone: ""
-                }
+                name: "",
+                phone: ""
             };
         }
+        handleChange(e){           
+            let nextState = {};
+            nextState[e.target.name] = e.target.value;
+
+            this.setState(nextState);
+        }
+
+        // handleClick(){
+        //     this.props.onInsert(this.state.contactData.name, this.state.contactData.phone);
+        //     this.setState({
+        //         name:"",
+        //         phone:""
+        //     });
+        // }
     render(){
         return (
             <div>
                 <p>
-                    <input type="text" name="name" placeholder="name" value={this.state.contactData.name}/>
-                    <input type="text" name="phone" placeholder="phone" value={this.state.contactData.phone}/>
-                    <button>
-                    Insert
-                    </button>
+                    <input type="text" name="name" placeholder="name" value={this.state.name} onChange={this.handleChange.bind(this)}/>
+                    <input type="text" name="phone" placeholder="phone" value={this.state.phone} onChange={this.handleChange.bind(this)}/>
+                    
+                    
                 </p>
             </div>
         );
